a842f2687af25c9bdf2d74aa7995ae15465f437b
lilydao11 <123123238+lilydao11@users.noreply.github.com>
2024-09-03 13:20:13 -0400

    Add files via upload

---
+++ assignment_1.m
@@ -0,0 +1,50 @@
+%Definition of the test function and its derivative
+test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
+test_derivative01 = @(x) 3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
+
+test_func_1 = [@(x) (x.^2) - 9; @(x) (2*x)];
+test_func = @(x) (x.^2) - 9;
+
+zeros = bisection_solver(test_func, 0, 6);
+test = bisection_solver(test_func01, 0, 10);
+test_1 = newton_solver(test_func_1, 4)
+
+function x = bisection_solver(fun,x_left,x_right)
+    x_mid = 10;
+    c = 11;
+    while abs(x_mid - c) > 10^-14
+        if fun(x_left) * fun(x_right) > 0 
+            quit
+        else
+            x_mid = (x_left + x_right) / 2;
+            if fun(x_left) * fun(x_mid) < 0
+                x_right = x_mid;
+                c = (x_left + x_right) / 2;
+            elseif fun(x_right) * fun(x_mid) < 0
+                x_left = x_mid;
+                c = (x_right + x_left) / 2;
+            elseif fun(x_mid) == 0
+                c = x_mid;
+                break
+            end
+        end
+    end
+    x = c;
+end
+
+
+%Note that fun(x) should output [f,dfdx], where dfdx is the derivative of f
+function z = newton_solver(fun,x0)
+    difference = 1;
+    x1 = x0;
+    while difference > 10^-14
+        x0 = x1;
+        funct = fun(x0);
+        x1 = x0 - funct(1) / funct(2);
+        difference = abs(x1 - x0);
+    end
+    z = x1;
+end
+function x = secant_solver(fun,x0, x1)
+%your code here
+end
