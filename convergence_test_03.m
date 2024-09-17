b5e9f49ae452f74b1acaf5c5ba40c690194386d0
nrandolin <nrand@olin.edu>
2024-09-17 11:13:42 -0400

    do stuff

--- convergence_test_03.m
+++ convergence_test_03.m
@@ -13,20 +13,20 @@
 % 
 % hold off
 
-% % Test Newton
-% clear
-% solver_flag = 2;
-% fun = @test_function;
-% 
-% filter_list = [1e-15, 1e-2,1e-14,1e-2,2];
-% x_guess0 = 1;
-% guess_list1 = linspace(0,33,1000);
-% guess_list2 = linspace(2,7,1000);
-%     
-% convergence_analysis(solver_flag, fun, ...
-% x_guess0, guess_list1, guess_list2, filter_list)
-% 
-% hold off
+% Test Newton
+clear
+solver_flag = 2;
+fun = @test_function;
+
+filter_list = [1e-15, 1e-2,1e-14,1e-2,2];
+x_guess0 = 1;
+guess_list1 = linspace(0,10,100);
+guess_list2 = linspace(2,7,100);
+    
+convergence_analysis(solver_flag, fun, ...
+x_guess0, guess_list1, guess_list2, filter_list)
+
+hold off
 
 % Test Secant
 clear
